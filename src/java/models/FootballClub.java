853a223ae5f6e194bf6a0c98e3f3826b49a2c448
jesseponkm <thuatdthe173070@fpt.edu.vn>
2024-05-22 10:07:42 +0700

    update models

--- src/java/models/FootballClub.java
+++ src/java/models/FootballClub.java
@@ -4,10 +4,89 @@
  */
 package models;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author admin
  */
 public class FootballClub {
-    
+
+    private int clubId;
+    private String clubName;
+    private String createBy;
+    private LocalDate createdDate;
+    private String updateBy;
+    private LocalDate lastUpdateDate;
+    private boolean isDeleted;
+
+    public FootballClub() {
+    }
+
+    public FootballClub(int clubId, String clubName, String createBy, LocalDate createdDate, String updateBy, LocalDate lastUpdateDate, boolean isDeleted) {
+        this.clubId = clubId;
+        this.clubName = clubName;
+        this.createBy = createBy;
+        this.createdDate = createdDate;
+        this.updateBy = updateBy;
+        this.lastUpdateDate = lastUpdateDate;
+        this.isDeleted = isDeleted;
+    }
+
+    public int getClubId() {
+        return clubId;
+    }
+
+    public void setClubId(int clubId) {
+        this.clubId = clubId;
+    }
+
+    public String getClubName() {
+        return clubName;
+    }
+
+    public void setClubName(String clubName) {
+        this.clubName = clubName;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public LocalDate getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(LocalDate lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public boolean isIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
 }
--- src/java/models/MatchType.java
+++ src/java/models/MatchType.java
@@ -4,10 +4,89 @@
  */
 package models;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author admin
  */
 public class MatchType {
-    
+
+    private int typeId;
+    private String name;
+    private String createBy;
+    private LocalDate createdDate;
+    private String updateBy;
+    private LocalDate lastUpdateDate;
+    private boolean isDeleted;
+
+    public MatchType() {
+    }
+
+    public MatchType(int typeId, String name, String createBy, LocalDate createdDate, String updateBy, LocalDate lastUpdateDate, boolean isDeleted) {
+        this.typeId = typeId;
+        this.name = name;
+        this.createBy = createBy;
+        this.createdDate = createdDate;
+        this.updateBy = updateBy;
+        this.lastUpdateDate = lastUpdateDate;
+        this.isDeleted = isDeleted;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public LocalDate getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDate createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public LocalDate getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(LocalDate lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public boolean isIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
 }
