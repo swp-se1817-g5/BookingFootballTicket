a96ae4c6280223c79571cc6153e6e803155ed7ba
Nguyễn Cảnh Dương <duongnche173192@fpt.edu.vn>
2024-06-03 12:51:35 +0700

    Add  in getFootballClubs

--- src/java/controllers/homePage/ManageHomePageServlet.java
+++ src/java/controllers/homePage/ManageHomePageServlet.java
@@ -63,7 +63,7 @@ public class ManageHomePageServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
 //        out.print(MatchDAO.INSTANCE.getMatches());
         request.setAttribute("getListMatches", MatchDAO.INSTANCE.getMatches());
-        request.setAttribute("getFootballClubs", FootballClubDAO.INSTANCE.getFootballClubs());
+        request.setAttribute("getFootballClubs", FootballClubDAO.INSTANCE.getFootballClubs(""));
         request.setAttribute("getMatches", MatchDAO.INSTANCE.getMatches());
 //        request.setAttribute("getStandsWithMinPrice", StandDAO.INSTANCE.getStandsWithMinPrice());
         
