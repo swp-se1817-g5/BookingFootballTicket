59840645067ad0edabacd6ac7971e39e7340f21d
anhquan2102 <quantahe170941@fpt.edu.vn>
2024-06-04 13:15:34 +0700

    update hashPassword, fix validate name, fix adduser

--- src/java/constant/IConstant.java
+++ src/java/constant/IConstant.java
@@ -13,5 +13,5 @@ public class IConstant {
     public static final String REGEX_NUMBER = "^\\d+$";
     public static final String REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$";
     public static final String REGEX_EMAIL = "^[\\w-\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
-    public static final String REGEX_NAME = "^[A-Za-z\\d\\s\\W]+$";
+    public static final String REGEX_NAME = "^(?!\\s*$)[A-Za-z\\d\\s\\W]+$";
 }
