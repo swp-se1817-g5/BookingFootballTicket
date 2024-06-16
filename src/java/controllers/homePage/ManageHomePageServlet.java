0b8085ffd0bda913e13fc1ca4dafece0e462cf67
Nguyễn Cảnh Dương <duongnche173192@fpt.edu.vn>
2024-06-16 15:37:00 +0700

    Update gitignore

--- src/java/controllers/homePage/ManageHomePageServlet.java
+++ src/java/controllers/homePage/ManageHomePageServlet.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controllers.home_page;
+package controllers.homePage;
 
 import dal.FootballClubDAO;
 import dal.MatchDAO;
--- src/java/controllers/manageNews/CreateNewNewsServlet.java
+++ src/java/controllers/manageNews/CreateNewNewsServlet.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controllers.manage_news;
+package controllers.manageNews;
 
 import dal.NewsDAO;
 import java.io.IOException;
