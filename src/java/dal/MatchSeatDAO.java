1b32fa7acda4475ba802ba5638ea2c61e3cc4763
jesseponkm <thuatdthe173070@fpt.edu.vn>
2024-07-06 22:46:15 +0700

    modified summaryCheckout

--- src/java/dal/MatchSeatDAO.java
+++ src/java/dal/MatchSeatDAO.java
@@ -62,7 +62,7 @@ public class MatchSeatDAO {
                 SeatArea seat = new SeatArea();
                 SeatClass seatClass = new SeatClass();
                 Stand stand = new Stand();
-                
+
                 stand.setStandId(rs.getInt("standId"));
                 stand.setStandName(rs.getString("standName"));
 
@@ -77,10 +77,10 @@ public class MatchSeatDAO {
                 seatClass.setSeatClassId(rs.getInt("seatClassId"));
                 seatClass.setSeatClassName(rs.getString("seatClassName"));
                 seat.setQuantity(rs.getInt("quantity"));
-                
+
                 seat.setSeatClass(seatClass);
                 seat.setStand(stand);
-                
+
                 matchSeat.setPrice(rs.getBigDecimal("price"));
                 matchSeat.setSeatarea(seat);
                 matchSeat.setAvailability(rs.getInt("availability"));
@@ -89,6 +89,65 @@ public class MatchSeatDAO {
         } catch (SQLException e) {
         }
         return matchSeats;
+    }
+
+    public MatchSeat getMatchSeatbyMatchSeatId(int matchSeatId) {
+        MatchSeat matchSeat = new MatchSeat();
+        String sql = """
+                     SELECT 
+                     ms.matchSeatId,
+                     ms.matchId,
+                     ms.seatId,
+                     ms.availability,
+                     sa.seatId,
+                     st.standId as standId,
+                     st.standName as standName,
+                     sa.seatName,
+                     ms.price,
+                     sc.price as seatClassPrice,
+                     sc.seatClassId,
+                     sc.seatClassName,
+                     sa.quantity
+                     FROM MatchSeat ms
+                     join SeatArea sa on sa.seatId = ms.seatId
+                     join Stand st on st.standId = sa.standId
+                     join SeatClass sc on sc.seatClassId = sa.seatClassId
+                     WHERE matchSeatId = ? """;
+
+        try (PreparedStatement ps = con.prepareStatement(sql);) {
+            ps.setInt(1, matchSeatId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                SeatArea seat = new SeatArea();
+                SeatClass seatClass = new SeatClass();
+                Stand stand = new Stand();
+
+                stand.setStandId(rs.getInt("standId"));
+                stand.setStandName(rs.getString("standName"));
+
+                matchSeat.setMatchSeatId(rs.getInt("matchSeatId"));
+
+                matchSeat.setMatch(rs.getInt("matchId"));
+
+                seat.setSeatId(rs.getInt("seatId"));
+                seat.setSeatName(rs.getString("seatName"));
+
+                seatClass.setPrice(rs.getBigDecimal("seatClassPrice"));
+                seatClass.setSeatClassId(rs.getInt("seatClassId"));
+                seatClass.setSeatClassName(rs.getString("seatClassName"));
+                seat.setQuantity(rs.getInt("quantity"));
+
+                seat.setSeatClass(seatClass);
+                seat.setStand(stand);
+
+                matchSeat.setPrice(rs.getBigDecimal("price"));
+                matchSeat.setSeatarea(seat);
+                matchSeat.setAvailability(rs.getInt("availability"));
+            }
+        } catch (SQLException e) {
+        }
+        return matchSeat;
     }
 
     public static void main(String[] args) {
